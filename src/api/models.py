864d476cd2a22d585ae5b6e24a48f5ce93e9585a
Edi Javier <126507385+edijavier99@users.noreply.github.com>
2023-11-01 13:41:32 +0000

    models and routes done

--- src/api/models.py
+++ src/api/models.py
@@ -1,12 +1,33 @@
 from flask_sqlalchemy import SQLAlchemy
 
+import enum
+from sqlalchemy import Enum,ForeignKey,Float
+from datetime import datetime
+
+class myEnum(enum.Enum):
+    colchones = "colchones"
+    canapes = "canapes"
+    cabeceros = "cabeceros"
+    armarios = "armarios"
+    sofas = "sofas"
+    mesas = "mesas"
+    sillas = "sillas"
+    packs = "packs"
+
+
 db = SQLAlchemy()
 
 class User(db.Model):
+    __tablename__ = "user"
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    password = db.Column(db.String(1000), unique=False, nullable=False)
+    name = db.Column(db.String(40), nullable=False)
+    surname = db.Column(db.String(40), nullable=False)
+    image = db.Column(db.String(300), nullable=False)
+    location = db.Column(db.String(40), nullable=False)
+    carrito= db.Column(db.ARRAY(db.String(400)))
+
 
     def __repr__(self):
         return f'<User {self.email}>'
@@ -15,5 +36,56 @@ class User(db.Model):
         return {
             "id": self.id,
             "email": self.email,
-            # do not serialize the password, its a security breach
+            "name" : self.name ,
+            "surname" : self.surname ,
+            "image" : self.image ,
+            "location" : self.location,
+            "carrito": self.carrito,
         }
+    
+class Item(db.Model):
+    __tablename__ = "item"
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120), nullable=False)
+    category = db.Column(Enum(myEnum))
+    description = db.Column(db.String(1000), nullable=False)
+    location = db.Column(db.String(250), nullable=True)
+    publishing_date = db.Column(db.String(10), nullable=False)
+    image = db.Column(db.String(2000),nullable=True) 
+    counter = db.Column(db.Integer, nullable=True, default=0)
+    price = db.Column(db.Integer, nullable=True, default=0)
+    profundidad = db.Column(db.Integer, nullable=True, default=0)
+    capacidad = db.Column(db.Integer, nullable=True, default=0)
+    altura = db.Column(db.Integer, nullable=True, default=0)
+    grosor = db.Column(db.Integer, nullable=True, default=0)
+    firmeza = db.Column(db.Integer, nullable=True, default=0)
+    color = db.Column(db.String, nullable=True)
+    ancho = db.Column(db.Integer, nullable=True, default=0)
+    longitud = db.Column(db.Integer, nullable=True, default=0)
+    codigo = db.Column(db.String, nullable=True)
+
+    def __repr__(self):
+        return f'<Item {self.id}>'
+      
+    def serialize(self):
+       return {
+            "id": self.id,
+            "title": self.title,
+            "category": self.category.value if self.category else None,
+            "description": self.description,
+            "location":self.location,
+            "publishing_date": self.publishing_date,
+            "price": self.price,
+            "image": self.image,
+            "counter": self.counter,
+            "profundidad": self.profundidad,
+            "capacidad": self.capacidad,
+            "altura": self.altura,
+            "grosor": self.grosor, 
+            "firmeza": self.firmeza,
+            "color" : self.color,
+            "ancho" : self.ancho,
+            "longitud" : self.longitud,
+            "codigo" : self.codigo
+
+    }
--- src/api/routes.py
+++ src/api/routes.py
@@ -2,8 +2,15 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from api.models import db, User,Item
 from api.utils import generate_sitemap, APIException
+import bcrypt
+import stripe
+import os 
+from flask_jwt_extended import (
+    JWTManager, jwt_required, create_access_token
+)
+
 
 api = Blueprint('api', __name__)
 
@@ -16,3 +23,156 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@api.route('/create-user', methods=['POST'])
+def create_user():
+    data = request.get_json()
+    if data is None:
+        response_body = {
+            "msg" : "Body should be passed with request"
+        }
+        return jsonify(response_body),400
+    
+    required_fields = ["name", "surname", "email", "password"]
+    for field in required_fields:
+        if field not in data:
+            response_body = {
+                "msg": f"{field.capitalize()} should be in request"
+            }
+            return jsonify(response_body), 400
+        
+    password_hash = bcrypt.hashpw(data["password"].encode('utf-8'), bcrypt.gensalt())
+    password_hex = password_hash.hex()
+    new_user= User(email = data["email"], name=data["name"], surname=data["surname"], password=password_hex ,location=data["location"], image=data["image"])
+    db.session.add(new_user)   
+    db.session.commit()
+    return jsonify({"msg": "user has been added"}),200
+
+
+@api.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    email = data.get("loginEmail", None)
+    password = data.get("loginPassword", None)
+
+    if email is None or password is None:
+        response_body = {
+            "msg": "Email and password are required"
+        }
+        return jsonify(response_body), 400
+
+    user = User.query.filter_by(email=email).first()
+    if user is None:
+        response_body = {
+            "msg": "User not found"
+        }
+        return jsonify(response_body), 404
+
+    stored_password_hex = user.password 
+    stored_password_bytes = bytes.fromhex(stored_password_hex)
+
+    if bcrypt.checkpw(password.encode('utf-8'), stored_password_bytes):
+        logged = "Successfully logged"
+        access_token = create_access_token(identity=user.id)
+
+        return jsonify({"loginOK": logged, "token": access_token, "user_id": user.id, "username": user.name, "email": user.email}),200
+    else:
+        response_body = {
+            "msg": "Invalid password"
+        }
+        return jsonify(response_body), 400
+
+@api.route('/upload-item', methods=['POST'])
+def upload_item():
+    data = request.get_json()
+    if data is None:
+        response_body = {
+            "msg" : "Body should be passed with request"
+        }
+        return jsonify(response_body),400
+    
+    required_fields = ["title", "description", "publishing_date", "image", "category", "price","color"]
+    for field in required_fields:
+        if field not in data:
+            response_body = {
+                "msg": f"{field.capitalize()} should be in request"
+            }
+            return jsonify(response_body), 400
+        
+    new_item= Item(title = data["title"], description=data["description"], price=data["price"], category=data["category"], color=data["color"], publishing_date=data["publishing_date"], image=data["image"], altura=data["altura"], profundidad=data["profundidad"],longitud=data["longitud"],codigo=data["codigo"])
+    db.session.add(new_item)   
+    db.session.commit()
+    return jsonify({"msg": "Item has been added"}),200
+
+
+@api.route('/payment-stripe', methods=['POST'])
+def get_stripe_details():
+    stripe.api_key = os.environ.get("STRIPE_API_KEY")
+
+    try:
+        data = request.get_json()
+        amount = data["amount"]*100
+
+        charge = stripe.Charge.create(
+            amount=amount,
+            currency='usd',
+            source='tok_visa', 
+            description='Ejemplo de cargo',
+        )
+        return jsonify(charge),200
+
+    except stripe.error.CardError as e:
+        return jsonify({"error": str(e)}), 400 
+    
+
+@api.route('/single-item/<int:id>',methods=['GET'])
+def get_single_item(id):
+    item = Item.query.get(id)
+    return jsonify(item.serialize()),200
+
+@api.route('/items', methods=['GET'])
+def get_all_items():
+    category = request.args.get("category")
+    all_items = Item.query.filter_by(category=category)
+    all_items = list(map(lambda x: x.serialize(), all_items))
+
+    return jsonify(all_items), 200
+
+
+@api.route('user/<int:id>',methods=['PUT'])
+def update_user(id):
+    data = request.get_json()
+
+    if data is None:
+        response_body={
+            "msg":"body should be passed with request parameters"
+        }
+        return jsonify(response_body),400
+    
+    update_user= User.query.get(id)
+    
+    if "email" in data:
+        update_user.email = data["email"]
+    if "password" in data:
+        update_user.password = data["password"]
+    if "carrito" in data:
+        update_user.carrito = [str(item) for item in data["carrito"]]
+    db.session.commit()
+
+    return jsonify({"msg":"user updated"}),200
+
+
+@api.route('/user/<int:id>',methods=["GET"])
+def get_single_user(id):
+    user = User.query.get(id)
+    return jsonify(user.serialize()), 200
+
+
+@api.route('/search',methods=["GET"])
+def handle_search():
+    search_query = request.args.get('query') 
+    results = Item.query.filter(Item.title.like(f'%{search_query}%')).all()
+
+    serialized_results = [{'id': item.id, 'name': item.title} for item in results]
+
+    return jsonify({'results': serialized_results})
